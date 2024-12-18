0e6da4f8f7782d6b266f6cd746c8d226a2a7dd2e
Patrik Alm <almpatrik@yahoo.se>
2024-12-18 13:25:38 +0100

    Implemented DataBaseConnection

--- src/main/java/io/github/patrikalm/maintenance/DataBaseConnection.java
+++ src/main/java/io/github/patrikalm/maintenance/DataBaseConnection.java
@@ -1,11 +1,14 @@
 package io.github.patrikalm.maintenance;
 
+import java.io.IOException;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 import java.util.Properties;
 
 public class DataBaseConnection {
@@ -15,12 +18,23 @@ public class DataBaseConnection {
 
         Properties properties = new Properties();
 
-        Reader in = Files.newBufferedReader(Path.of();
+        try(
+                Reader in = Files.newBufferedReader(Path.of("src/main/resources/database.properties"), StandardCharsets.UTF_8);
+                ){
 
+            properties.load(in);
+            System.out.println("File read");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // return DriverManager.getConnection(url, name, password);
+        System.out.println("Read Properties");
+        String url = properties.getProperty("jdbc.url");
+        String username = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
 
+        return DriverManager.getConnection(url, username, password);
 
     }
 
